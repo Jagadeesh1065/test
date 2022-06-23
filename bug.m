a8b88dba0eeb460f344b96d65d096df67983bf27
Jagadeesh1065 <jagadeesh.srungarapati007@gmail.com>
2022-06-23 14:52:33 +0530

    new file added

---
+++ bug.m
@@ -0,0 +1,8 @@
+layer = [key(1)*layer(1) key(2)*layer(2) key(3)*layer(3)]
+final(:,:,1)=layer(1)
+final(:,:,2)=layer(2)
+final(:,:,3)=layer(3)
+final=final/100;
+figure (1);
+imshow(image)
+figure (2)
